d1f3d4b618724dd4d652077c7a86f6eab4d79ed5
Nolan Kovacik <noltron000@gmail.com>
2019-11-22 14:49:44 -0800

    [mov] euler's and factorial from old

--- source/shtam.ts
+++ source/shtam.ts
@@ -18,7 +18,7 @@ export const E: number = ε
 
 /*
 	CHALLENGE 4:
-	define some useful functions.
+	define some useful angle functions.
 */
 
 export const radToDeg = (radians: number): number => {
@@ -27,4 +27,77 @@ export const radToDeg = (radians: number): number => {
 
 export const degToRad = (degrees: number): number => {
 	return degrees * π / 180
+}
+
+/*
+	CHALLENGE A:
+	implement factorial.
+*/
+
+export const factorial = (n: number): number => {
+	let product: number
+
+	if (n === 1 || n === 0) {
+		// base case
+		product = n
+	} else if (n > 1) {
+		// larger positive number
+		product = n * factorial(n - 1)
+	} else {
+		// some other wierd case
+		product = NaN
+	}
+
+	// finally, return result
+	return product
+}
+
+export const factorialMemo = (n: number): number[] => {
+	let productList: number[] = []
+
+	const memo = (i: number): number => {
+		let product: number
+
+		if (i === 0 || i === 1) {
+			// base case
+			product = 1
+		} else if (n > 1) {
+			// larger positive number
+			product = i * productList[i - 1]
+		} else {
+			// some other wierd case
+			product = NaN
+		}
+
+		// finally, return result
+		return product
+	}
+
+	// get a range of numbers from 0 to n+1
+	const range: number[] = Array.from(Array(n + 1).keys())
+
+	// find factorial for each item
+	range.forEach((i: number) => {
+		const product: number = memo(i)
+		productList.push(product)
+	})
+
+	// finally, return resulting list
+	return productList
+}
+
+/* 
+	CHALLENGE B:
+	implement euler's number w/precision.
+*/
+
+const eulersNumber = (precision: number): number => {
+	let e: number = 0
+	// need every factorial from 0 to n
+	const factorialList: number[] = factorialMemo(precision)
+	// loop through each factorial and add 1/n! to e
+	factorialList.forEach((n: number) => {
+		e += 1 / n
+	})
+	return e
 }
