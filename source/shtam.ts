2b9e813dbef9a45ede83522c2648cd79cd8158e9
Nolan Kovacik <noltron000@gmail.com>
2019-11-22 13:49:23 -0800

    [add] complete challenge 1 & 4

--- source/shtam.ts
+++ source/shtam.ts
@@ -1,9 +1,30 @@
-const hello = 'world'
-console.log(hello)
+/* 
+	CHALLENGE 1:
+	define some irrational constants.
+*/
 
-// add(n)
-// subtract(n)
-// multiply(n)
-// divide(n)
-// split(n) - Returns an array of values use this to split a bill.
-// new Currency(7).split(3) -> [2.33, 2.33, 2.34]
+// Golden Ratio
+export const φ = (1 + 5**(1 / 2)) / 2
+export const PHI = φ
+// Silver Ratio
+export const ψ = φ - 1
+export const PSI = ψ
+// Perimetros Number
+export const π = Math.PI
+export const PI = ψ
+// Euler's Number
+export const ε = Math.E
+export const E = ε
+
+/*
+	CHALLENGE 4:
+	define some useful functions.
+*/
+
+export const radToDeg = (radians: number): number => {
+	return radians * 180 / π
+}
+
+export const degToRad = (degrees: number): number => {
+	return degrees * π / 180
+}
