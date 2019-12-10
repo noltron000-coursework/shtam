4b4d1eac485476eff2495178cdd15bc9184c0d8c
Nolan Kovacik <noltron000@gmail.com>
2019-12-10 14:50:52 -0800

    [del] old code

--- source/shtam.ts
+++ source/shtam.ts
@@ -269,3 +269,18 @@ export const eulersNumber = (precision: number = 100): number => {
 	})
 	return e
 }
+
+/*
+	OLD CONTENTS
+*/
+
+// intFormat(amount, countryCode, style)
+// Number.prototype.toIntlCurrency = function(
+// 	locales: string,
+// 	currencyType: string,
+// ): string {
+// 	return new Intl.NumberFormat(locales, {
+// 		style: 'currency',
+// 		currency: currencyType,
+// 	}).format(this)
+// }
